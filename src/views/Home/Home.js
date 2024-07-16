bf50b098ededbf4d097f83a4420fbc68666a1439
TejasBTECHWAla <tdongare1@gmail.com>
2024-07-16 17:33:00 +0530

    added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,7 +4,24 @@ import NewsArticle from '../../components/NewsArticle/NewsArticle'
 import "./Home.css"
 
 function Home() {
-   
+    const [news , setNews] = useState([])
+    const [searchQuery, setSearchQuery] = useState()
+
+    const loadNews = async ()=>{
+        try{
+            const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-07-15&to=2024-07-15&sortBy=popularity&apiKey=a12ad439c4264bc196fe1f504f7e8250`);
+
+            setNews(response.data.articles)
+        }
+        catch(error){
+            console.log(error)
+        }
+        }
+
+        useEffect(()=>{
+            loadNews()
+        },[])
+
         useEffect(()=>{
             loadNews()
         },[searchQuery])
@@ -13,21 +30,7 @@ function Home() {
 
 
   return (
-   <div className=''>
-    <h1 className='heading'>News App</h1>
-    <input type="text" placeholder='click here to search' value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} className='search'/>
-    <div className='container'>
-    {
-        news.map((newsArticle, index)=>{
-            const {author, title, description, url, urlToImage, publishedAt, content} = newsArticle
-
-            return(
-                <NewsArticle author={author} title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} content={content}/>
-            )
-        })
-    }  
-    </div>  
-   </div>
+   
   )
 }
 
