952232293c0d5aac800e747e33c0f052fa7d0db0
TejasBTECHWAla <tdongare1@gmail.com>
2024-07-16 17:32:00 +0530

    load function added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,24 +4,7 @@ import NewsArticle from '../../components/NewsArticle/NewsArticle'
 import "./Home.css"
 
 function Home() {
-    const [news , setNews] = useState([])
-    const [searchQuery, setSearchQuery] = useState()
-
-    const loadNews = async ()=>{
-        try{
-            const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-07-15&to=2024-07-15&sortBy=popularity&apiKey=a12ad439c4264bc196fe1f504f7e8250`);
-
-            setNews(response.data.articles)
-        }
-        catch(error){
-            console.log(error)
-        }
-        }
-
-        useEffect(()=>{
-            loadNews()
-        },[])
-
+   
         useEffect(()=>{
             loadNews()
         },[searchQuery])
