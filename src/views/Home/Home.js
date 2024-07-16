168a2b18f92a239d68ea7e16587253c2ae6045da
TejasBTECHWAla <tdongare1@gmail.com>
2024-07-16 17:36:05 +0530

    mapping done

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -30,7 +30,21 @@ function Home() {
 
 
   return (
-   
+    <div className=''>
+    <h1 className='heading'>News App</h1>
+    <input type="text" placeholder='click here to search' value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} className='search'/>
+    <div className='container'>
+    {
+        news.map((newsArticle, index)=>{
+            const {author, title, description, url, urlToImage, publishedAt, content} = newsArticle
+
+            return(
+                <NewsArticle author={author} title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} content={content}/>
+            )
+        })
+    }  
+    </div>  
+   </div>
   )
 }
 
